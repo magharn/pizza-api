ffb26e05bc68d0449caa3857bc3fab8b4294501c
Sergio Gtz <sergio.oscar.gb@gmail.com>
2018-04-01 22:12:21 -0500

    project has been structured and two endpoints were implemented

--- api.js
+++ api.js
@@ -3,13 +3,26 @@
 const Api = require('claudia-api-builder');
 const api = new Api();
 
-api.get('/pizzas', ()=>{
-    return [
-        'Capricciosa',
-        'Quattro Formaggi',
-        'Napoletana',
-        'Margherita'
-    ]
+const getPizzas = require('./handlers/get-pizzas');
+const createOrder = require('./handlers/create-order');
+
+api.get('/', () => 'Welcome to Pizza API');
+
+api.get('/pizzas', () => {
+    return getPizzas();
+});
+
+api.get('/pizzas/{id}', (request) => {
+    return getPizzas(request.pathParams.id);
+}, {
+    error: 404
+});
+
+api.post('/orders', (request) => {
+    return createOrder(request.body);
+}, {
+    success : 201,
+    error : 400
 });
 
 module.exports = api;
