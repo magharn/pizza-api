c9fede1d9f6ef0548ee2376f5cc1d48bc583cf72
Sergio Gtz <sergio.oscar.gb@gmail.com>
2018-04-08 14:17:22 -0500

    update order function was updated to support DynamoDB interaction

--- handlers/update-order.js
+++ handlers/update-order.js
@@ -1,11 +1,31 @@
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient();
 
-function updateOrder(id, updates) {
-    if (!id || !updates)
-        throw new Error('Order ID and updates object are required for updating the order');
+function updateOrder(orderId, options) {
+    if (!options || !options.pizza || !options.address)
+        throw new Error('Both pizza and address are required to update an order');
 
-    return {
-        message : `Order ${id} was successfully updated`
-    };
+
+    return docClient.update({
+        TableName : 'pizza-orders',
+        Key : {
+            orderId: orderId
+        },
+        UpdateExpression: "set pizza = :p, address=:a",
+        ExpressionAttributeValues:{
+            ":a":options.address,
+            ":p": options.pizza
+        },
+        ReturnValues:"ALL_NEW"        
+    }).promise()
+     .then((result) => {
+        console.log('Order is updated', result);
+        return result.Attributes
+     })
+     .catch((updateError) => {
+         console.log(`Oops, order is not updated :(`, updateError);
+         throw updateError
+     })
 }
 
 module.exports = updateOrder;
